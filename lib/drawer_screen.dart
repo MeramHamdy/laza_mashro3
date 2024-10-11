0713c261f0abef5ccf0fbbcb6579994cbbfa89cd
MeramHamdy <hamdymeram@gmail.com>
2024-10-11 14:20:44 +0300

    About&Privacy

--- lib/drawer_screen.dart
+++ lib/drawer_screen.dart
@@ -1,87 +1,116 @@
 import 'package:flutter/material.dart';
+import 'package:laza_mashro3/screens/about_app.dart';
 import 'package:laza_mashro3/screens/home_screen.dart';
 import 'package:laza_mashro3/screens/nav_bar_screen.dart';
 import 'package:laza_mashro3/theme_color/themes_provider.dart';
+import 'package:laza_mashro3/screens/privacy_policy.dart';
 import 'package:provider/provider.dart';
 
 class DrawerScreen extends StatelessWidget {
   const DrawerScreen({super.key});
-
 
   @override
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<ThemesProvider>(context);
 
     return Scaffold(
-
-     body: Padding(
-        padding: const EdgeInsets.all(15.0),
-
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 50,),
+        body: Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
+        SizedBox(
+          height: 50,
+        ),
         CircleAvatar(
-        backgroundColor: Colors.deepPurple,
-            child: IconButton(onPressed: (){
-              Navigator.of(context).pop();
-
-            },
+            backgroundColor: Colors.deepPurple,
+            child: IconButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
               icon: Icon(Icons.menu),
             )),
-        SizedBox(height: 20,),
-        ListTile(
-            leading:CircleAvatar(
-              child: Icon(Icons.person),
-            ),
-            title: Text("UserName",),
-            subtitle: Text("mera@gmail.com"),
+        SizedBox(
+          height: 20,
         ),
-              SizedBox(height:20 ,),
-              Row(
-                    children: [
-                      Icon(Icons.light_mode_outlined,color: Colors.deepPurple,),
-                      SizedBox(width: 10,),
-                      Text(
-                        themeProvider.isDarkMode? 'Light Mode':'Dark Mode',
-                        style: TextStyle(fontSize: 15),
-                      ),
-                      Spacer(),
-                      Switch(
-                        value: themeProvider.isDarkMode,
-                        onChanged: (value) {themeProvider.toggleThem();},
-                        activeColor: Colors.deepPurple,
-                        activeTrackColor: Colors.white,
-                      )
-                    ],
-                  ),
-              Roow(icoon: Icons.card_giftcard, txt: "Order"),
-              Spacer(),
-              Roow(icoon: Icons.logout, txt: "Logout"),
-              SizedBox(height:50 ,),
-
-
-            ]),
-      )  );
+        ListTile(
+          leading: CircleAvatar(
+            child: Icon(Icons.person),
+          ),
+          title: Text(
+            "UserName",
+          ),
+          subtitle: Text("mera@gmail.com"),
+        ),
+        SizedBox(
+          height: 20,
+        ),
+        Row(
+          children: [
+            Icon(
+              Icons.light_mode_outlined,
+              color: Colors.deepPurple,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              themeProvider.isDarkMode ? 'Light Mode' : 'Dark Mode',
+              style: TextStyle(fontSize: 15),
+            ),
+            Spacer(),
+            Switch(
+              value: themeProvider.isDarkMode,
+              onChanged: (value) {
+                themeProvider.toggleThem();
+              },
+              activeColor: Colors.deepPurple,
+              activeTrackColor: Colors.white,
+            )
+          ],
+        ),
+        Roow(icoon: Icons.card_giftcard, txt: "Order",widgetName: AboutApp(),),
+        Spacer(),
+        Roow(icoon: Icons.privacy_tip_outlined, txt: "Privacy & Policy",widgetName:PrivacyPolicy(),),
+        Roow(icoon: Icons.info_outline, txt: "About App",widgetName: AboutApp(),),
+        Roow(icoon: Icons.logout, txt: "Logout",widgetName: AboutApp(),),
+        SizedBox(
+          height: 50,
+        ),
+      ]),
+    ));
   }
 }
+
 class Roow extends StatelessWidget {
-  IconData icoon;
-  String txt;
-   Roow({required this.icoon,required this.txt});
+  final IconData icoon;
+  final String txt;
+  final Widget widgetName;
+
+
+  Roow({required this.icoon, required this.txt,required this.widgetName});
 
   @override
   Widget build(BuildContext context) {
-    return    Padding(
-      padding: const EdgeInsets.all(15.0),
-      child: Row(
-        children: [
-          Icon(icoon,color: Colors.deepPurple,),
-          SizedBox(width: 10,),
-          Text(txt,style: TextStyle(fontSize: 15),),
-        ],
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: GestureDetector(
+        onTap:  (){  Navigator.push(context,
+        MaterialPageRoute(builder: (context) => widgetName));},
+        child: Row(
+          children: [
+            Icon(
+              icoon,
+              color: Colors.deepPurple,
+            ),
+            SizedBox(
+              width: 10,
+            ),
+            Text(
+              txt,
+              style: TextStyle(fontSize: 15),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
-
---
+++ lib/screens/privacy_policy.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:laza_mashro3/widgets/about_privacy.dart';
+
+class PrivacyPolicy extends StatelessWidget {
+  const PrivacyPolicy({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return AboutPrivacy(
+        screenName: 'Privacy Policy',
+        txt1: 'Privacy Policy for Laza\n\n',
+        txt2: '''*Data Collection* 
+- We may collect personal information such as your name, email address, and payment details when you create an account or make a purchase.
+- We also collect non-personal information, such as usage data, to improve our app’s services.\n
+        
+*Data Usage*
+- Personal information is used to process transactions, enhance user experience, and communicate important updates regarding your account or orders.
+- Non-personal data helps us understand app usage trends and optimize functionality.\n
+        
+*Data Security*
+- We implement robust security measures to protect your personal information from unauthorized access, alteration, or misuse.\n
+        
+*Data Sharing*
+- We do not sell or rent your personal data to third parties.
+- We may share your information with trusted partners who help operate our app and conduct business, provided they maintain confidentiality.\n
+        
+*User Rights*
+- You have the right to access, correct, or delete your personal information at any time. Contact us at [contact email] to exercise these rights.\n
+        
+*Policy Updates*
+- We may update this Privacy Policy periodically.
+- Any changes will be communicated through updates posted on our app.''');
+  }
+}
