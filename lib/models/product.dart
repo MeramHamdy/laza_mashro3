c85a9ddb3543f5a22155f27b5b130b08475d1e18
MeramHamdy <hamdymeram@gmail.com>
2024-10-13 19:07:28 +0300

    id&reviews

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,18 +1,31 @@
-
 import 'package:json_annotation/json_annotation.dart';
+import 'package:laza_mashro3/widgets/reviews.dart';
 
 part 'product.g.dart';
 
 @JsonSerializable()
 class Product {
+  final int id;
   final String title;
   final List<String> images;
   final double price;
   final double rating;
+  final List<Reviews> reviews;
   final String category;
   final String description;
 
 //constructor
-  Product({required this.title, required this.images, required this.price,required this.category,required this.description, required this.rating});
-  factory Product.fromJson(Map<String,dynamic> json) => _$ProductFromJson(json);
+  Product({
+    required this.id,
+    required this.title,
+    required this.images,
+    required this.price,
+    required this.rating,
+    required this.reviews,
+    required this.category,
+    required this.description,
+  });
+
+  factory Product.fromJson(Map<String, dynamic> json) =>
+      _$ProductFromJson(json);
 }
